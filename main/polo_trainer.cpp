de4f30a1e00946d22f7eca85069b7f912bfccd00
automind <mail@automind.top>
2025-03-20 02:14:50 +0700

    MP3 pipeline!

--- main/polo_trainer.cpp
+++ main/polo_trainer.cpp
@@ -528,6 +528,7 @@ IRAM_ATTR static esp_err_t dac_output_with_dither(float gain , uint8_t* buf , ui
 #include "audio_event_iface.h"
 #include "audio_mem.h"
 #include "audio_common.h"
+#include "pwm_stream.h"
 #include "mp3_decoder.h"
 
 IRAM_ATTR esp_err_t play_wav(char* fp) {
@@ -654,23 +655,26 @@ IRAM_ATTR int mp3_music_read_cb(audio_element_handle_t el , char* buf , int len 
     // int read_size = file_marker.end - file_marker.start - file_marker.pos;
     FILE* fh = (FILE*)ctx;
 
-
     // vTaskDelay(100);
 
     size_t read_size = fread(buf , sizeof(char) , len , fh);
-    ESP_LOGI("mp3_music_read_cb" , "reading %d bytes, read %u" , len , read_size);
+    ESP_LOGI("read_cb" , "read %u" , read_size);
 
      // int read_size = 0;
     if (read_size == 0) {
         return AEL_IO_DONE;
-    } else if (read_size < len) {
-        read_size = len;
     }
     // memcpy(buf , file_marker.start + file_marker.pos , read_size);
     // file_marker.pos += read_size;
+
+    // vTaskDelay(10);
+
+
     return read_size;
 }
 
+#define CONFIG_PWM_LEFT_OUTPUT_GPIO_NUM GPIO_NUM_17
+#define CONFIG_PWM_RIGHT_OUTPUT_GPIO_NUM GPIO_NUM_18
 
 IRAM_ATTR int play_mp3(char* fp) {
 
@@ -684,28 +688,57 @@ IRAM_ATTR int play_mp3(char* fp) {
 
 
     audio_pipeline_handle_t pipeline;
-    audio_element_handle_t mp3_decoder;
+    audio_element_handle_t mp3_decoder , output_stream_writer;
     // audio_element_handle_t i2s_stream_writer, mp3_decoder;
     ESP_LOGI("play_mp3" , "creating audio pipeline");
     audio_pipeline_cfg_t pipeline_cfg = DEFAULT_AUDIO_PIPELINE_CONFIG();
+    // pipeline_cfg.rb_size = 2048;
     pipeline = audio_pipeline_init(&pipeline_cfg);
     mem_assert(pipeline);
 
+    ESP_LOGI(TAG , "[2.1] Create output stream to write data to codec chip");
+    pwm_stream_cfg_t pwm_cfg = PWM_STREAM_CFG_DEFAULT();
+    pwm_cfg.pwm_config.gpio_num_left = CONFIG_PWM_LEFT_OUTPUT_GPIO_NUM;
+    pwm_cfg.pwm_config.gpio_num_right = CONFIG_PWM_RIGHT_OUTPUT_GPIO_NUM;
+    output_stream_writer = pwm_stream_init(&pwm_cfg);
 
-    ESP_LOGI("play_mp3" , "creating mp3 decoder");
+    // ESP_LOGI("play_mp3" , "creating mp3 decoder");
+    // mp3_decoder_cfg_t mp3_cfg = DEFAULT_MP3_DECODER_CONFIG();
+    // mp3_cfg.stack_in_ext = false;
+    // mp3_cfg.id3_parse_enable = true;
+    // mp3_decoder = mp3_decoder_init(&mp3_cfg);
+    // audio_element_set_read_cb(mp3_decoder , mp3_music_read_cb , (void*)fh);
+
+    ESP_LOGI(TAG , "create mp3 decoder");
     mp3_decoder_cfg_t mp3_cfg = DEFAULT_MP3_DECODER_CONFIG();
+    mp3_cfg.stack_in_ext = false;
     mp3_decoder = mp3_decoder_init(&mp3_cfg);
     audio_element_set_read_cb(mp3_decoder , mp3_music_read_cb , (void*)fh);
 
 
+
+
+
+
     ESP_LOGI("play_mp3" , "registering all elements to audio pipeline");
+    // audio_pipeline_register(pipeline , mp3_decoder , "mp3");
+    // ESP_LOGI(TAG, "[2.3] Register all elements to audio pipeline");
     audio_pipeline_register(pipeline , mp3_decoder , "mp3");
+    audio_pipeline_register(pipeline , output_stream_writer , "output");
+
     // audio_pipeline_register(pipeline, i2s_stream_writer, "i2s");
 
 
     ESP_LOGI("play_mp3" , "link together [mp3_music_read_cb]-->mp3_decoder");
-    const char* link_tag[1] = {"mp3"};
-    audio_pipeline_link(pipeline , &link_tag[0] , 1);
+    // const char* link_tag[1] = {"mp3"};
+    // audio_pipeline_link(pipeline , &link_tag[0] , 1);
+
+
+    // ESP_LOGI(TAG, "[2.4] Link it together [mp3_music_read_cb]-->mp3_decoder-->output_stream-->[pa_chip]");
+    const char* link_tag[2] = {"mp3", "output"};
+    audio_pipeline_link(pipeline , &link_tag[0] , 2);
+
+
 
 
     ESP_LOGI("play_mp3" , "set up  event listener");
@@ -717,21 +750,93 @@ IRAM_ATTR int play_mp3(char* fp) {
 
     ESP_ERROR_CHECK(audio_pipeline_run(pipeline));
 
-    audio_element_info_t music_info = {0};
 
-    audio_element_getinfo(mp3_decoder , &music_info);
-    ESP_LOGI("play_mp3" , "music info from mp3 decoder, sample_rates=%d, bits=%d, ch=%d" , music_info.sample_rates , music_info.bits , music_info.channels);
+    // vTaskDelay(1000);
+
+    // audio_element_info_t music_info = {0};
+
+    // audio_element_getinfo(mp3_decoder , &music_info);
+    // ESP_LOGI("play_mp3" , "music info from mp3 decoder, sample_rates=%d, bits=%d, ch=%d, kbps=%d, duration=%d" , music_info.sample_rates , music_info.bits , music_info.channels , music_info.bps / 1024 , music_info.duration);
 
 
+
+    // vTaskDelay(1000);
 
 
     while (1) {
 
         audio_event_iface_msg_t msg;
         esp_err_t ret = audio_event_iface_listen(evt , &msg , portMAX_DELAY);
+        ESP_LOGE("play_mp3" , "event:::::: %d" , msg.cmd);
         if (ret != ESP_OK) {
             continue;
         }
+
+        if (msg.source_type == AUDIO_ELEMENT_TYPE_ELEMENT && msg.source == (void*)mp3_decoder
+            && msg.cmd == AEL_MSG_CMD_REPORT_MUSIC_INFO) {
+            audio_element_info_t music_info = {0};
+            audio_element_getinfo(mp3_decoder , &music_info);
+            ESP_LOGI("play_mp3" , "music info from mp3 decoder, sample_rates=%d, bits=%d, ch=%d, kbps=%d, duration=%d" , music_info.sample_rates , music_info.bits , music_info.channels , music_info.bps / 1024 , music_info.duration);
+            audio_element_set_music_info(output_stream_writer , music_info.sample_rates , music_info.channels , music_info.bits);
+            pwm_stream_set_clk(output_stream_writer , music_info.sample_rates , music_info.bits , music_info.channels);
+                // ESP_LOGI(TAG , "[ * ] Receive music info from mp3 decoder, sample_rates=%d, bits=%d, ch=%d" ,
+                    // music_info.sample_rates , music_info.bits , music_info.channels);
+                // i2s_stream_set_clk(i2s_stream_writer , music_info.sample_rates , music_info.bits , music_info.channels);
+
+            // audio_event_iface_msg_t msggg = {0};
+            // msggg.cmd = AEL_MSG_CMD_RESUME;
+            // audio_event_iface_cmd(evt , &msggg);
+            continue;
+
+        }
+
+
+
+        if (msg.source_type == AUDIO_ELEMENT_TYPE_ELEMENT && msg.source == (void*)output_stream_writer
+            && msg.cmd == AEL_MSG_CMD_REPORT_STATUS
+            && (((int)msg.data == AEL_STATUS_STATE_STOPPED) || ((int)msg.data == AEL_STATUS_STATE_FINISHED))) {
+                // audio_element_info_t music_info = {0};
+            // audio_element_getinfo(mp3_decoder , &music_info);
+
+
+            // audio_element_state_t el_state = audio_element_get_state(mp3_decoder);
+            // audioestat
+
+            // audio_element_report_status(mp3_decoder , )
+
+
+            // ESP_LOGI("play_mp3" , "el_state=%d" , (int)msg.data);
+            ESP_LOGI(TAG , "[ * ] Stop event received");
+
+                // ESP_LOGI(TAG , "[ * ] Receive music info from mp3 decoder, sample_rates=%d, bits=%d, ch=%d" ,
+                    // music_info.sample_rates , music_info.bits , music_info.channels);
+                // i2s_stream_set_clk(i2s_stream_writer , music_info.sample_rates , music_info.bits , music_info.channels);
+            break;
+        }
+
+
+        if (msg.source_type == AUDIO_ELEMENT_TYPE_ELEMENT && msg.source == (void*)mp3_decoder
+            && msg.cmd == AEL_MSG_CMD_REPORT_POSITION) {
+            // audio_element_info_t music_info = {0};
+            // audio_element_getinfo(mp3_decoder , &music_info);
+
+
+            // audio_element_state_t el_state = audio_element_get_(mp3_decoder);
+
+            // audio_element_report_pos(mp3_decoder);
+
+            // ESP_LOGI("play_mp3" , "pos???????");
+
+                // ESP_LOGI(TAG , "[ * ] Receive music info from mp3 decoder, sample_rates=%d, bits=%d, ch=%d" ,
+                    // music_info.sample_rates , music_info.bits , music_info.channels);
+                // i2s_stream_set_clk(i2s_stream_writer , music_info.sample_rates , music_info.bits , music_info.channels);
+            continue;
+        }
+                    // audio_pipeline_resume(pipeline);
+                    // audio_event_iface_msg_t msg;
+                    // msg.cmd
+                        // audio_event_iface_sendout()
+
     }
 
         // if (msg.source_type == AUDIO_ELEMENT_TYPE_ELEMENT && msg.source == (void*)mp3_decoder
